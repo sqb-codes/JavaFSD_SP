f44ace931b1c737f8efcef52512abd6feb551c83
sqb-codes <150049400+sqb-codes@users.noreply.github.com>
2025-07-03 10:22:42 +0530

    Add files via upload

--- jira/src/main/java/com/jira/jira/security/SecurityConfig.java
+++ jira/src/main/java/com/jira/jira/security/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.jira.jira.security;
 
+import com.jira.jira.service.JWTFilter;
 import com.jira.jira.service.UserDetailServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,12 +16,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
 
     private final UserDetailServiceImpl userDetailsService;
+    @Autowired private JWTFilter jwtFilter;
 
     public SecurityConfig(UserDetailServiceImpl userDetailsService) {
         this.userDetailsService = userDetailsService;
@@ -32,6 +36,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
